acf1e5bafb39c15a7eff672cea7cdefb37895db2
mihailgudkov <moiiwa@icloud.com>
2021-08-29 12:41:32 +0300

    Commit to check the proper work of caching

--- app_python/src/views.py
+++ app_python/src/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
+
+
 def index(request):
     return render(request, '../templates/index.html')
